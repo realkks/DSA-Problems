5ac684a745d0275abc97bf06d2f6b5a4ec21e676
= <=>
2025-05-21 22:41:10 +0530

    First commit of this project

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // This is a simple Java program that prints a welcome message to the console.
+        System.out.println("Hello and welcome!");
+    }
+}
